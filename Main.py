bc312a74892d2d16ed3079b07f4519c4b9335ad2
taonga07 <thomas-2415@digilocal.org.uk>
2021-12-04 16:42:35 +0000

    got code working

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot
 # import pygame
 
 GameWorld = [a.split() for a in open("GameWorlds/World.txt").read().split('\n')]
-playerposx, playerposy = 1, 1
+playerposx, playerposy = 2, 1
 
 for i in range(60):
     rot_i = (pi/4) + radians(i - 30)
@@ -11,7 +11,7 @@ for i in range(60):
     tsin, tcos = 0.02*sin(rot_i), 0.02*cos(rot_i)
     while True:
         x, y, n = x+tcos, y+tsin, n+1
-        if GameWorld[int(x)][int(y)] != 0:
+        if GameWorld[int(x)][int(y)] != '0':
             height = 1/(0.02*n)
             print(height)
             break
