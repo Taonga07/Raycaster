720a13522b32bda486abec948c4d6c3af4c0cfdc
taonga07 <thomas-2415@digilocal.org.uk>
2021-12-09 19:02:07 +0000

    code cleanup & started with player movement

--- Main.py
+++ Main.py
@@ -3,26 +3,39 @@ from time import sleep
 import pygame
 
 pygame.init()  # pylint: disable=E1101
-SCREEEN_SIZE = 300
+SCREEEN_SIZE, VIEWSIZE = 300, 60
 window = pygame.display.set_mode((SCREEEN_SIZE, SCREEEN_SIZE))
 clock = pygame.time.Clock()
 GameWorld = [a.split() for a in open("GameWorlds/World.txt").read().split("\n")]
 playerposx, playerposy = 2, 1
 
+def fancy_maths():
+    rot_i = (pi / 4) + radians(i - 30)
+    x, y, n = playerposx, playerposy, 0
+    tsin, tcos = 0.02 * sin(rot_i), 0.02 * cos(rot_i)
+    while True:
+        x, y, n = x + tcos, y + tsin, n + 1
+        if GameWorld[int(x)][int(y)] != "0":
+            height = (1 / (0.02 * n)) * SCREEEN_SIZE
+            return height
+
 while True:
+    print(playerposx, playerposy)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    for i in range(60):
-        rot_i = (pi / 4) + radians(i - 30)
-        x, y, n = playerposx, playerposy, 0
-        tsin, tcos = 0.02 * sin(rot_i), 0.02 * cos(rot_i)
-        while True:
-            x, y, n = x + tcos, y + tsin, n + 1
-            if GameWorld[int(x)][int(y)] != "0":
-                height = (1 / (0.02 * n)) * SCREEEN_SIZE
-                break
-        pygame.draw.line(window,(125, 125, 125), (i+(i*(SCREEEN_SIZE/60)), (SCREEEN_SIZE / 2) + (height / 2)),
-                        (i+(i*(SCREEEN_SIZE/60)), (SCREEEN_SIZE / 2) - (height / 2)))
-        pygame.display.flip()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                playerposx += 1
+            if event.key == pygame.K_DOWN:
+                playerposx -= 1
+            if event.key == pygame.K_LEFT:
+                playerposy += 1
+            if event.key == pygame.K_RIGHT:
+                playerposy -= 1
+    for i in range(VIEWSIZE):
+        height = fancy_maths()
+        linex, lineofsety = i+(i*(SCREEEN_SIZE/VIEWSIZE)), (SCREEEN_SIZE / 2) + (height / 2)
+        pygame.draw.line(window,(125, 125, 125), (linex, lineofsety), (linex,lineofsety))
+    pygame.display.flip()
     clock.tick()
